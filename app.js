356fe867ee56f155e9186c832721e0e7b4afbbf1
Chosen <chosendev2000@gmail.com>
2024-01-13 13:07:55 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+// app.use('/', Router)
+
+const PORT = process.env.PORT || 7000;
+const MongoDB_String = process.env.MongoDB_String;
+
+async function connectMongoDB() {
+    try {
+        await mongoose.connect(`${MongoDB_String}`);
+        console.log('MongoDB Connected Successfully')
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+connectMongoDB();
+
+app.listen(PORT, ()=> {
+    console.log(`App Listening on Port ${PORT}`)
+});
